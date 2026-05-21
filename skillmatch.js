84792b3698d5ea45430ac1922de1c82aa66903c5
Carolina Duarte Neves <carolinaduarteneves@gmail.com>
2026-05-21 17:03:28 -0300

    feat(RF01): cria perfil do candidato

---
+++ skillmatch.js
@@ -0,0 +1,19 @@
+// ============================================================
+// DADOS PRINCIPAIS
+// ============================================================
+
+const candidato = {
+  nome: "Carolina",
+  area: "Front-End",
+  habilidades: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "DOM",
+    "GitHub",
+    "Responsividade",
+    "Arrays",
+    "Kanban",
+  ],
+  experienciaMeses: 4,
+};
