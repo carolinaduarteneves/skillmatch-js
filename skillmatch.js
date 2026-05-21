5ebd0c0f517c3c8dd0e838a111d9db0dd7224e4c
Carolina Duarte Neves <carolinaduarteneves@gmail.com>
2026-05-21 17:18:20 -0300

    feat(RF09): cria classe Vaga

--- skillmatch.js
+++ skillmatch.js
@@ -74,3 +74,22 @@ const vagas = [
     modalidade: "Remoto",
   },
 ];
+
+// ============================================================
+// CLASSES
+// ============================================================
+
+class Vaga {
+  constructor(id, empresa, cargo, requisitos, salario, modalidade) {
+    this.id = id;
+    this.empresa = empresa;
+    this.cargo = cargo;
+    this.requisitos = requisitos;
+    this.salario = salario;
+    this.modalidade = modalidade;
+  }
+
+  exibirResumo() {
+    return `${this.cargo} na empresa ${this.empresa}`;
+  }
+}
