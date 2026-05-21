19b899d749ee7a497d921e374a0a9d29e700777d
Carolina Duarte Neves <carolinaduarteneves@gmail.com>
2026-05-21 17:06:25 -0300

    feat(RF02): cria lista de vagas

--- skillmatch.js
+++ skillmatch.js
@@ -17,3 +17,60 @@ const candidato = {
   ],
   experienciaMeses: 4,
 };
+
+const vagas = [
+  {
+    id: 1,
+    empresa: "Dev Solutions",
+    cargo: "Desenvolvedor Front-End Júnior",
+    requisitos: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "DOM",
+      "GitHub",
+      "Responsividade",
+      "Arrays",
+      "Kanban",
+    ],
+    salario: 2000,
+    modalidade: "Remoto",
+  },
+  {
+    id: 2,
+    empresa: "PixelCode",
+    cargo: "Estagiário em Desenvolvimento Web",
+    requisitos: ["HTML", "CSS", "Objetos", "Funções", "Bootstrap"],
+    salario: 2500,
+    modalidade: "Híbrido",
+  },
+  {
+    id: 3,
+    empresa: "Tech Systems",
+    cargo: "Assistente de Suporte Técnico",
+    requisitos: [
+      "Hardware",
+      "Algoritmos",
+      "Noções de Redes",
+      "Resolução de Problemas",
+    ],
+    salario: 2200,
+    modalidade: "Presencial",
+  },
+  {
+    id: 4,
+    empresa: "PixelWave Studio",
+    cargo: "Desenvolvedor Web Trainee",
+    requisitos: [
+      "JavaScript",
+      "DOM",
+      "Arrays",
+      "Responsividade",
+      "React",
+      "Lógica de Programação",
+      "Node.js",
+    ],
+    salario: 3100,
+    modalidade: "Remoto",
+  },
+];
